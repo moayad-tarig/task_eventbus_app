081796586e23aea77d0f7a9f7b3aa47492774cc0
Moayad Tarig <software.moayad@gmail.com>
2024-10-04 06:10:36 +0400

    starter project

---
+++ README.md
@@ -0,0 +1,3 @@
+# task_eventbus_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/task_eventbus_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.task_eventbus_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
